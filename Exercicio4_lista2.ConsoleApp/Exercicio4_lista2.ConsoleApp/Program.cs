23cb03cda20e01d24616b37b8fef8c1e106aa6d0
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-07 19:20:47 -0300

    Add files via upload

---
+++ Exercicio4_lista2.ConsoleApp/Exercicio4_lista2.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio4_lista2.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            int numero = System.Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O numero " + numero + " é par!");
+            }
+            else
+            {
+                Console.WriteLine("O numero " + numero + " é ímpar!");
+            }
+        }
+    }
+}
