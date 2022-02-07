22ff109c06c8a1b8d77d2446005691d721de7648
PedrouNunes <90865544+PedrouNunes@users.noreply.github.com>
2022-02-07 19:21:26 -0300

    Add files via upload

---
+++ Exercicio5_Lista2.ConsoleApp/Exercicio5_Lista2.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio5_Lista2.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro valor: ");
+            double A = System.Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            double B = System.Convert.ToDouble(Console.ReadLine());
+
+            double C;
+
+            if (A == B)
+            {             
+                 C = A + B;
+                Console.WriteLine("A equação foi uma soma! O resultado é: " + C);
+            }
+            else
+            {
+                C = A * B;
+                Console.WriteLine("A equação foi uma multiplicação! O resultado é: " + C);
+            }
+        }
+    }
+}
